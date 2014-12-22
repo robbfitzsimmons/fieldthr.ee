7f51d2001dc45b1a8e17fb3b8f34e3b111b3c9be
Robb Fitzsimmons <robbfitzsimmons@gmail.com>
2014-12-21 21:47:31 -0500

    Adding groovy transitioning CSS background

---
+++ packages/telescope-theme-fieldthree/package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  summary: "Telescope FIELD3 theme",
+  version: '0.1.0',
+  name: "telescope-theme-fieldthree"
+});
+
+Package.onUse(function (api) {
+
+  api.use(['fourseven:scss', 'telescope-theme-hubble'], ['client']);
+
+  api.addFiles([
+    'lib/client/stylesheets/screen.scss',
+    ], ['client']);
+
+});
